49bdf77e0a6150a99fabce4e135797e3a6fa5760
Maks <petrenkomaks405@gmail.com>
2019-08-10 15:12:26 +0300

    Created a program that asks for name, age, gender, weight and height

--- src/HealthKey.java
+++ src/HealthKey.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class HealthKey {
     public static void main (String []args){
+
+        System.out.println("You are welcomed by the program HealthKey!");
+
+        Scanner s = new Scanner (System.in);
+        System.out.print("Your name : ");
+        String a = s.nextLine();
+        System.out.print("Your age : ");
+        int b = s.nextInt();
+        System.out.print("Your gender : ");
+        boolean c = s.nextBoolean();
+        System.out.print("Your height : ");
+        float d = s.nextFloat();
+        System.out.print("Your weight : ");
+        double e = s.nextDouble();
+
         
     }
 }
